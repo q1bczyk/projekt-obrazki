bffb842cfc7c6e9cef37f33a80ff081b748c40b5
Kenny <Kamil.pacula1@gmail.com>
2023-05-30 23:18:13 +0200

    Dodanie funkcji dla języka angielskiego

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from PIL import ImageTk, Image
-from load_file import loadFile
 from functools import partial
 from gray_scale import convertToGrayscale, displayGrayscale
 from PIL import Image, ImageOps
-
 
 # --------------- GLOBAL
 
 is_black_and_white = False
 original_image = None
-
 
 # --------------- FUNCTIONS
 
@@ -30,7 +27,6 @@ def loadFile(container):
         image_label.photo = photo
         image_label.pack(fill='both', expand=True)
         original_image = resized_image.copy()
-
 
 def convertToBlackAndWhite(container):
     global is_black_and_white, original_image
@@ -52,18 +48,46 @@ def convertToBlackAndWhite(container):
         image_label.photo = photo
         is_black_and_white = True
 
-
-
+def changeLanguage():
+    current_language = changeToEnglishButton['text']
+    if current_language == "Angielski":
+        changeToEnglishButton.config(text="Polski")
+        loadFileButton.config(text="Load Image")
+        filterButton.config(text="Filter")
+        saveFileButton.config(text="Save Image")
+        blackAndWhiteButton.config(text="Black & White")
+        normalizeButton.config(text="Normalization")
+        masksOptions = ["Masks", "Mask 1", "Mask 2", "Mask 3", "Mask 4"]
+        choosenMasksOption.set(masksOptions[0])
+        normalizationOptions = ["Normalization", "Absolute", "Scaled", "Trimmed"]
+        choosenNormalizationOption.set(normalizationOptions[0])
+        normalizeButton["menu"].delete(0, "end")
+        for option in normalizationOptions:
+            normalizeButton["menu"].add_command(label=option, command=tk._setit(choosenNormalizationOption, option))
+    else:
+        changeToEnglishButton.config(text="Angielski")
+        loadFileButton.config(text="Wczytaj zdjęcie")
+        filterButton.config(text="Filtruj")
+        saveFileButton.config(text="Zapisz zdjęcie")
+        blackAndWhiteButton.config(text="Czarno-Białe")
+        normalizeButton.config(text="Normalizacja")
+        masksOptions = ["Maski", "maska 1", "maska 2", "maska 3", "maska 4"]
+        choosenMasksOption.set(masksOptions[0])
+        normalizationOptions = ["Normalizacja", "Bezwzględna", "Skalowana", "Z obcięciem"]
+        choosenNormalizationOption.set(normalizationOptions[0])
+        normalizeButton["menu"].delete(0, "end")
+        for option in normalizationOptions:
+            normalizeButton["menu"].add_command(label=option, command=tk._setit(choosenNormalizationOption, option))
 
 # --------------- GUI window
 
-root = tk.Tk();
+root = tk.Tk()
 
-masksOptions = ["Maski","maska 1", "maska 2", "maska 3", "maska 4"]
+masksOptions = ["Maski", "maska 1", "maska 2", "maska 3", "maska 4"]
 choosenMasksOption = tk.StringVar(root)
 choosenMasksOption.set(masksOptions[0])
 
-normalizationOptions = ["Normalizacja","Bezwzgledna", "Skalowana", "Z obcieciem"]
+normalizationOptions = ["Normalizacja", "Bezwzględna", "Skalowana", "Z obcięciem"]
 choosenNormalizationOption = tk.StringVar(root)
 choosenNormalizationOption.set(normalizationOptions[0])
 
@@ -99,10 +123,10 @@ filteredPhotoFrame.grid(row=1, column=2, padx=10, pady=10)
 
 # --------------- BUTTONS
 
-changeToEnglishButton = ttk.Button(loadImageFrame, text="Angielski", style='custom.TButton')
+changeToEnglishButton = ttk.Button(loadImageFrame, text="Angielski", style='custom.TButton', command=changeLanguage)
 changeToEnglishButton.grid(row=1, column=1, padx=10, pady=10)
 
-loadFileButton = ttk.Button(loadImageFrame, text="Wczytaj zdjęcie", style='custom.TButton')
+loadFileButton = ttk.Button(loadImageFrame, text="Wczytaj zdjęcie", style='custom.TButton', command=partial(loadFile, originalPhotoFrame))
 loadFileButton.grid(row=2, column=1, padx=10, pady=10)
 
 filterButton = ttk.Button(loadImageFrame, text="Filtruj", style='custom.TButton')
@@ -111,7 +135,7 @@ filterButton.grid(row=3, column=1, padx=10, pady=10)
 saveFileButton = ttk.Button(loadImageFrame, text="Zapisz zdjęcie", style='custom.TButton')
 saveFileButton.grid(row=4, column=1, padx=10, pady=10)
 
-blackAndWhiteButton = ttk.Button(buttonsFrame, text="Czarno-Białe", style='custom.TButton')
+blackAndWhiteButton = ttk.Button(buttonsFrame, text="Czarno-Białe", style='custom.TButton', command=partial(convertToBlackAndWhite, originalPhotoFrame))
 blackAndWhiteButton.grid(row=2, column=1, padx=10, pady=10)
 
 normalizeButton = ttk.OptionMenu(buttonsFrame, choosenNormalizationOption, *normalizationOptions, style='custom.TButton')
@@ -120,13 +144,7 @@ normalizeButton.grid(row=2, column=2, padx=10, pady=10)
 masksMenu = ttk.OptionMenu(buttonsFrame, choosenMasksOption, *masksOptions, style='custom.TButton')
 masksMenu.grid(row=2, column=3, padx=10, pady=10)
 
-# --------------- CODE
-
-loadFileButton.config(command=partial(loadFile, originalPhotoFrame))
-blackAndWhiteButton.config(command=partial(convertToBlackAndWhite, originalPhotoFrame))
-
 # --------------- GRID CONFIGURATIONS
 
-root.resizable(width = False, height = False)
+root.resizable(width=False, height=False)
 root.mainloop()
-
